be031c9511206b2c00a864dccd7569323aebc414
hoppantut <hoppantut@users.noreply.github.com>
2014-03-04 18:24:59 +0100

    Added basic script for setting up symbolic links and backing up any old files

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+log=`basename $0`.log
+exec >  >(tee -a ${log})
+exec 2> >(tee -a ${log} >&2)
+
+echo -e "\n`date`"
+for dotfile in `find . -type f \( -iname ".*" -not -iname ".*.*" -not -iname ".gitignore" \) -printf "%f\n"` ; do
+    echo "Looking at ${dotfile}"
+    if [ -f $HOME/${dotfile} ] && [ "`md5sum $HOME/${dotfile} | awk '{ print $1 }'`" = "`md5sum ${dotfile} | awk '{ print $1 }'`" ]; then
+        echo "  $HOME/$(basename ${dotfile}) is identical and will not be replaced."
+    else
+        echo "  $HOME/$(basename ${dotfile}) does not exist or is different."
+        if [ -f $HOME/${dotfile} ]; then
+            echo "    Backing up $HOME/${dotfile} as $HOME/${dotfile}.backup."
+            mv  $HOME/${dotfile} $HOME/${dotfile}.backup
+        fi
+        echo "    Making symbolic link..."
+        ln -sv $(realpath ${dotfile}) $HOME/$(basename ${dotfile})
+    fi
+done
